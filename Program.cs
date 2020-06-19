788fa70c038e042b8189032a354cf79ded5aa21f
truemarin <65551447+truemarin@users.noreply.github.com>
2020-06-19 21:56:23 +0200

    Add program

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace BingoCreator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string bingoPath = Console.ReadLine();
+
+            string[] lines = File.ReadAllLines(@bingoPath);
+
+            List<int> usedLines = new List<int>();
+
+            Random rng = new Random();
+
+            int[,] bingoCard = new int[4, 4];
+            int lineIndex;
+
+            Console.WriteLine("\n");
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write("\n");
+
+                for (int j = 0; j < 4; j++)
+                {
+                    do
+                    {
+                        lineIndex = rng.Next(lines.Length + 1);
+                        bingoCard[i, j] = lineIndex;
+                    }
+                    while (usedLines.Contains(lineIndex));
+
+                    usedLines.Add(lineIndex);
+                    Console.Write($" {lineIndex}");
+                }
+            }
+
+            Console.WriteLine("\n\n");
+
+
+        }
+    }
+}
